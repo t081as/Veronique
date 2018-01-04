0d42e84617e3416a0ba61e9b0ef238b2399279c6
Tobias Koch <t.koch@tk-software.de>
2018-01-04 11:09:06 +0100

    Adding writer command implementation

---
+++ Veronique/Veronique/Writers/EnvironmentVariableWriterCommand.cs
@@ -0,0 +1,52 @@
+﻿#region GNU General Public License 3
+// Veronique - Easy versioning for software projects
+// Copyright (C) 2017  Tobias Koch
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+#region Namespaces
+using System;
+using Veronique.Utilities;
+#endregion
+
+namespace Veronique.Writers
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="IWriterCommand"/> interface setting an environment variable.
+    /// </summary>
+    [WriterCommand("set-env-var")]
+    public class EnvironmentVariableWriterCommand : IWriterCommand
+    {
+        #region Methods
+
+        /// <summary>
+        /// Writes a definition based variable to a destination.
+        /// </summary>
+        /// <param name="parameters">The parameters given to this writer.</param>
+        /// <exception cref="ArgumentNullException"><c>parameters</c> is null.</exception>
+        /// <exception cref="ArgumentNullException">A parameter is null.</exception>
+        /// <exception cref="ArgumentException">The number of arguments is invalid.</exception>
+        /// <exception cref="ArgumentException">An argument is invalid.</exception>
+        /// <exception cref="ApplicationException">An error occured during the operation.</exception>
+        public void Write(string[] parameters)
+        {
+            Parameter.Check(parameters, 2, 2);
+
+            Environment.SetEnvironmentVariable(parameters[0], parameters[1]);
+        }
+
+        #endregion
+    }
+}
